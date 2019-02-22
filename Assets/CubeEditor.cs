441dd59a7a137482eb56a67e4e042468ef06df61
suatozcan <email@example.com>
2019-02-22 09:55:29 +0300

    Add enemy and more cubes.

--- Assets/CubeEditor.cs
+++ Assets/CubeEditor.cs
@@ -3,8 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [ExecuteInEditMode]
-public class EditorSnap : MonoBehaviour {
+[SelectionBase]
+public class CubeEditor : MonoBehaviour {
+
     [SerializeField] [Range(1f, 20f)] float gridSize = 10f;
+    TextMesh textMesh;
 
     // Use this for initialization
     void Start () {
@@ -18,5 +21,10 @@ public class EditorSnap : MonoBehaviour {
         snapPos.z = Mathf.RoundToInt(transform.position.z / gridSize) * gridSize;
 
         transform.position = new Vector3(snapPos.x, 0f, snapPos.z);
+
+        textMesh = GetComponentInChildren<TextMesh>();
+        string labelText = snapPos.x / gridSize + "," + snapPos.z / gridSize;
+        textMesh.text = labelText;
+        gameObject.name = labelText;
     }
 }
